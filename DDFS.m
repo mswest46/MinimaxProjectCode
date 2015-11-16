601d5cd9e35b0dc9bcc5cdcf3afa67c3b997ed7b
Michael West <mswest46@gmail.com>
2015-11-16 18:23:54 +0000

    working pretty well, some minor issues, only with the one test graph

--- DDFS.m
+++ DDFS.m
@@ -1,5 +1,5 @@
 
-function output = DDFS(adjacency_matrix, height, r, g, xy)
+function bottleneck_node = DDFS(adjacency_matrix, height, r, g, xy)
 close all
 gplot(adjacency_matrix,xy,'-*')
 axis([0,7,-1,8]);
@@ -7,101 +7,170 @@ hold on;
 
 num_nodes = size(adjacency_matrix,1);
 level = @(node_index) height(node_index);
+green_parent = zeros(1,num_nodes);
+red_parent = zeros(1,num_nodes);
+node_visited = zeros(1, num_nodes);
+edge_used = zeros(num_nodes);
 ownership = zeros(1,num_nodes); % 1 = red, 2 = green
 green_position = g;
-red_parents = zeros(1,num_nodes);
-green_parents = zeros(1,num_nodes);
 red_position = r;
-searched = zeros(num_nodes);
+barrier = g;
+bottleneck_found = 0;
 
 k = 0;
-while true
-    
-    % case. 
-    % 1 no collision, red moves first.
-    % 2 no collision, green moves.
-    % 3 collision, green searching
-    % 4 collsion, red searching
-    % 5 end collision, proceed or return. 
-    collision = (red_position == green_position);
-    collision_node = red_position;
-    
+while ~(level(red_position) == 0 && level(green_position) == 0)
     1;
-    k = k+1;
-    % plot graph
-    h1 = viscircles(xy(red_position,:),.25,'Color','red');
-    h2 = viscircles(xy(green_position,:),.2,'Color','green');
-    red_xy = xy(ownership == 1,:);
-    if ~isempty(red_xy)
-        x = red_xy(:,1);
-        y = red_xy(:,2);
-        plot(x,y,'r*');
-    end
-    green_xy = xy(ownership == 2,:);
-    if ~isempty(green_xy)
-        x = green_xy(:,1);
-        y = green_xy(:,2);
-        plot(x,y,'g*');
-    end
-    1;
-    pause;
     
-    if ~collision
-        
-        
-        if level(red_position)>=level(green_position)
-            neighbors = find(adjacency_matrix(red_position,:));
-            % advance red
-            1;
-            children = neighbors(level(neighbors)<level(red_position)...
-                & ~searched(neighbors));
-            if ~isempty(children)
-                searched(red_position) = 1;
-                best_kid = children(1);
-                red_parents(best_kid) = red_position;
-                ownership(red_position) = 1;
-                searched(red_position) = 1;
-                red_position = best_kid;
-            else
-                %backtrack
-                red_position = red_parents(red_position);
-            end
-        else
-            neighbors = find(adjacency_matrix(green_position,:));
-            % advance green
-            1;
-            children = neighbors(level(neighbors)<level(green_position)...
-                & ~searched(neighbors));
-            
-            
-            if ~isempty(children)
-                
-                best_kid = children(1);
-                searched(best_kid) = 1;
-                ownership(green_position) = 2;
-                green_parents(best_kid) = green_position;
-                green_position = best_kid;
-            else
-                %backtrack
-                green_position = green_parents(green_position);
-            end
-        end
-        
-    else
-        if red_position < green_position
-            collision = 0; % we've found a way around.
-        elseif
-            
-            % backtrack
-            red_position = red_parents(red_position);
-        end
-        
+    while level(red_position)>=level(green_position)
         
         % plot graph
-        delete(h1);
-        delete(h2);
+        
+        if k
+            delete(h1);
+            delete(h2);
+            delete(h3);
+         
+        end
+        k=1;
+        h1 = viscircles(xy(red_position,:),.25,'Color','red');
+        h2 = viscircles(xy(green_position,:),.2,'Color','green');
+        h3 = viscircles(xy(barrier,:),.3,'Color','black');
+        red_xy = xy(ownership == 1,:);
+        if ~isempty(red_xy)
+            x = red_xy(:,1);
+            y = red_xy(:,2);
+            plot(x,y,'r*');
+        end
+        green_xy = xy(ownership == 2,:);
+        if ~isempty(green_xy)
+            x = green_xy(:,1);
+            y = green_xy(:,2);
+            plot(x,y,'g*');
+        end
+        1;
+        pause;
+        
+        
+        if red_position == green_position
+            green_position = green_parent(green_position);
+            break
+        end
+        
+        
+        ownership(red_position) = 1;
+        node_visited(red_position) = 1;
+        unused_edges = (adjacency_matrix & ~edge_used);
+        unused_neighbors = find(unused_edges(red_position,:));
+        unused_children = unused_neighbors(...
+            height(unused_neighbors) < height(red_position));
+        flag = 0;
+        for u = unused_children
+            edge_used(red_position, u) = 1;
+            edge_used(u, red_position) = 1;
+            if u == green_position
+                1;
+                %we've met green, back green up 1.
+                green_position = green_parent(green_position);
+                red_parent(u) = red_position;
+                red_position = u;
+                flag = 1;
+                break
+            elseif ~node_visited(u)
+                red_parent(u) = red_position;
+                red_position = u;
+                flag = 1;
+                break
+            end
+        end
+        if flag
+            break
+        end
+        % only get to here when when all of red_poistions's children have
+        % been visited already. we backtrack if we're not at the top, else
+        % we've found a bottleneck.
+        if red_position == r;
+            bottleneck_found = 1;
+            break
+        else
+            red_position = red_parent(red_position);
+            break
+            
+        end
+        
     end
     
+    if bottleneck_found
+        bottleneck_node = green_position;
+        break
+    end
     
-    
+    while(level(red_position)<level(green_position))
+        
+        % plot graph
+        if k
+            delete(h1);
+            delete(h2);
+            delete(h3);
+         
+        end
+        k=1;
+        h1 = viscircles(xy(red_position,:),.25,'Color','red');
+        h2 = viscircles(xy(green_position,:),.2,'Color','green');
+        h3 = viscircles(xy(barrier,:),.3,'Color','black');
+        red_xy = xy(ownership == 1,:);
+        if ~isempty(red_xy)
+            x = red_xy(:,1);
+            y = red_xy(:,2);
+            plot(x,y,'r*');
+        end
+        green_xy = xy(ownership == 2,:);
+        if ~isempty(green_xy)
+            x = green_xy(:,1);
+            y = green_xy(:,2);
+            plot(x,y,'g*');
+        end
+        1;
+        pause;
+        
+        
+        ownership(green_position) = 2;
+        unused_edges = (adjacency_matrix & ~edge_used);
+        unused_neighbors = find(unused_edges(green_position,:));
+        unused_children = unused_neighbors(...
+            height(unused_neighbors) < height(green_position));
+        1;
+        flag = 0;
+        for u = unused_children
+            edge_used(green_position, u) = 1;
+            edge_used(u, green_position) = 1;
+            
+            if ~node_visited(u)
+                green_parent(u) = green_position;
+                green_position = u;
+                flag = 1;
+                break
+            end
+        end
+        
+        if flag == 1
+            break
+        end
+        
+         1;
+        if ~(green_position==barrier)
+            green_position = green_parent(green_position);
+        else
+            barrier = red_position;
+            green_position = red_position;
+            red_position = red_parent(red_position);
+        end
+    end
 end
+
+
+
+
+
+end
+
+
