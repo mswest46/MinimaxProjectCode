e4fa35ab9594c9137333a68e7e3cb6ef96c3c3e3
Michael West <mswest46@gmail.com>
2015-11-19 09:12:48 +0000

    fix bug in hopcroft

--- hopcroft_karp.m
+++ hopcroft_karp.m
@@ -3,17 +3,21 @@ function [matching_size, matching_matrix] = hopcroft_karp(adjacency_matrix, xy)
 
 graph.adjacency_matrix = adjacency_matrix;
 
+
 if ~(isfield(graph, 'part1') && isfield(graph, 'part2'))
     [graph.part1, graph.part2] = bipartition(adjacency_matrix);
 end
 
 if nargin == 2
     graph_bool = 1;
-else 
+else
     graph_bool = 0;
 end
 
+graph.graph_bool = graph_bool;
+
 if graph_bool
+    graph.xy = xy;
     hold on;
     gplot(adjacency_matrix,xy);
     axis([-.25,1.25,-.25,1.25]);
@@ -26,6 +30,7 @@ graph.nNodes = length(graph.adjacency_matrix(:,1));
 graph.dummy = graph.nNodes + 1;
 
 graph.pair = graph.dummy*ones(1,graph.nNodes); %everything starts paired with dummy
+count = 0;
 
 while true
     [graph.layer, bool] = BFS(graph);
@@ -38,6 +43,7 @@ while true
         if (graph.pair(u)==graph.dummy)
             [bool,pair,layer] = DFS(graph,u);
             if bool % i.e. if u in aug path
+                count = count+1;
                 graph.pair = pair; % change the matching
                 graph.layer = layer; % update the layers
             end
@@ -50,11 +56,18 @@ matching = [1:graph.nNodes; matching];
 
 matching = matching(:,(matching(2,:)<graph.dummy));
 matching_matrix = sparse(matching(1,:),matching(2,:),1,graph.nNodes,graph.nNodes);
+if any(sum(matching_matrix,1)>1)
+    error('this aint a matching');
+end
+
 matching_size = length(matching(1,:));
 
 if graph_bool
     gplot(matching_matrix,xy,'-.y')
 end
+
+disp('count is')
+disp(count)
 
 % matching_matrix = zeros(graph.nNodes);
 %
@@ -74,37 +87,51 @@ end
 
 function [layer, bool] = BFS(graph)
 dummy = graph.dummy;
-queue = zeros(1,graph.nNodes);
+% queue = zeros(1,graph.nNodes);
+queue = [];
 qSpot = 0;
 layer = nan(1,graph.nNodes);
+count = 1;
 for u = graph.part1
-    1;
+    count = count+1;
     if graph.pair(u)==dummy
-        qSpot = qSpot+1;
+%         qSpot = qSpot+1;
         layer(u) = 0;
-        queue(qSpot) = u;
+%         queue(qSpot) = u;
+        queue = [queue,u];
         % put the free vxs of part1 in queue and give layer 0
     else layer(u) = inf;
         % otherwise not in layer
     end
+    
+    if graph.graph_bool
+        str = num2str(layer(u));
+        h(count) = text(graph.xy(u,1)+.1,graph.xy(u,2),str);
+        pause(.3);
+    end
+    
 end
+
+delete(h);
 
 layer(dummy) = inf;
 
-while qSpot>0
-    %     while ~isempty(queue)
-    u = queue(qSpot);
-    qSpot = qSpot - 1;
-    %         u = queue(end); % pop u
-    %         queue = queue(1:end-1);
+% while qSpot>0
+while ~isempty(queue)
+    %     u = queue(qSpot);
+    %     qSpot = qSpot - 1;
+    u = queue(end); % pop u
+    queue = queue(1:end-1);
     if layer(u) < layer(dummy) % if we haven't hit dummy yet. if we've hit dummy we've found minimal aug paths already
-        for v=find(graph.adjacency_matrix(:,u)) % u's neighbors
+        neighbors = find(graph.adjacency_matrix(:,u));
+        for i = 1:length(neighbors);
+            v = neighbors(i);
             if layer(graph.pair(v)) == inf % v's pair not hit yet
                 layer(graph.pair(v)) = layer(u) + 1;
-                n = length(graph.pair(v));
-                queue(qSpot+1: qSpot+n) = graph.pair(v);
-                qSpot = qSpot + n;
-                %                     queue = [queue, graph.pair(v)];
+                %                 n = length(graph.pair(v));
+                %                 queue(qSpot+1: qSpot+n) = graph.pair(v);
+                %                 qSpot = qSpot + n;
+                queue = [queue, graph.pair(v)];
                 % eventually we will have v's pair be dummy, in wich
                 % case we have an aug path from a free vx in u to a
                 % free vx in v with length 2*layer(dummy) - 1.
