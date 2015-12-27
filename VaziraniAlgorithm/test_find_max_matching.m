059e30ec71176b517c583aa3fb28a82679c1fc90
Michael West <mswest46@gmail.com>
2015-12-27 02:44:30 -0600

    add a few assertions

--- VaziraniAlgorithm/test_find_max_matching.m
+++ VaziraniAlgorithm/test_find_max_matching.m
@@ -76,16 +76,28 @@ distribution.type = 'custom';
 params = [0,.7,0,0,0,0,0,0,0,.2,.1];
 distribution.params = size_bias(params);
 num_nodes = 1000;
+dummy = num_nodes +1;
 
 for i = 1:10
     adjacency_matrix = create_configuration_model(num_nodes,distribution);
     pair = find_max_matching(adjacency_matrix);
     matching_size = sum(pair<length(pair)+1);
     matching = true;
+    
     for j = 1: num_nodes
-        if length(find(pair==j,2))>1
+        if length(find(pair==j,2))>1 % double matched a vertex
             matching = false;
+            break
         end
+        if pair(j)~=dummy && adjacency_matrix(j,pair(j))==0 % paired with something that is not an edge
+            matching = false;
+            break
+        end
+        if pair(j) ~= dummy && pair(pair(j))~=j
+            matching = false;
+            break
+        end
+            
     end
     assert(matching);
     disp([num2str(i), ' of 10 matchings tested']);
