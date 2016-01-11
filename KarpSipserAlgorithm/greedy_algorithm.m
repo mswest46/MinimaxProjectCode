6e12662ced0d253a5056014e1f87703ae8710a05
Michael West <mswest46@gmail.com>
2016-01-11 17:52:54 -0500

    clean up KS, uses shorcut pendant now

--- KarpSipserAlgorithm/greedy_algorithm.m
+++ KarpSipserAlgorithm/greedy_algorithm.m
@@ -1,4 +1,4 @@
-function pair = greedy_algorithm(adjacency_matrix)
+function [pair,core] = greedy_algorithm(adjacency_matrix)
 
 G = create_graph_struct_from_adjacency_matrix(adjacency_matrix);
 
@@ -11,32 +11,49 @@ pair = dummy*ones(1,num_nodes);
 removed_vxs = false(1,num_nodes);
 pendants = false(1,num_nodes);
 neighbors = G.neighbors;
-
-deg1_vxs = zeros(1,1000);
-deg1_spot = 0;
+phase = 1;
+matching_size = 0;
+dispstat('','init');
+shortcut_pendant = nan;
 
 for i = 1:num_nodes
     if isempty(neighbors{i})
         removed_vxs(i) = true;
     elseif length(neighbors{i}) == 1;
         pendants(i) = true;
+        shortcut_pendant = i;
     end
     
 end
 
 while ~all(removed_vxs) % the graph still has some vxs.
-    v = find(pendants,1);
-    if v
+    if ~isnan(shortcut_pendant) && pendants(shortcut_pendant)
+        v = shortcut_pendant;
+    else
+        v = find(pendants,1);
+    end
+    if ~isempty(v)
         u = neighbors{v}; % there is only one
     else
+        if phase ==1
+            phase = 2;
+            core = removed_vxs;
+        end
         v = find(~removed_vxs,1);
         u = neighbors{v}(randi(length(neighbors{v}),1));
     end
     
     pair(u) = v; pair(v) = u; % pair these
     
-    % chain reaction erase from this edge.
+    % display things.
+    matching_size = matching_size + 2;
+    if mod(matching_size,num_nodes/1000)==0
+        dispstat([num2str(100*matching_size/num_nodes),...
+            '% of nodes matched']);
+    end
     
+    
+    % chain reaction erase from this edge.
     queue = [u,v];
     %     queue = zeros(1,1000);
     %     q_spot = 2; % last entry
@@ -46,7 +63,10 @@ while ~all(removed_vxs) % the graph still has some vxs.
         o = queue(end);
         queue = queue(1:end-1);
         removed_vxs(o) = true;
-        pendants(o) = false;
+        if pendants(o)
+            pendants(o) = false;
+        end
+        
         for i = 1:length(neighbors{o})
             w = neighbors{o}(i);
             if ~removed_vxs(w)
@@ -55,6 +75,7 @@ while ~all(removed_vxs) % the graph still has some vxs.
                     queue = [queue,w];
                 elseif length(neighbors{w}) == 1;
                     pendants(w) = true;
+                    shortcut_pendant = w;
                 end
             end
         end
@@ -62,8 +83,8 @@ while ~all(removed_vxs) % the graph still has some vxs.
 end
 
 matching_size = sum(pair<dummy);
-disp(['greedy algorithm matched ', num2str(100*matching_size/num_nodes),...
-    '% of nodes']);
+dispstat(['greedy algorithm matched ', num2str(100*matching_size/num_nodes),...
+    '% of nodes'], 'keepthis');
 end
 
 % if degree 1 vx exists, pick one at random, match its edge, remove it
