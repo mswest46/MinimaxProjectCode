4172ffde2331089d3eb1d558fcb0e3e770cc8fcd
Michael West <mswest46@gmail.com>
2016-01-29 16:17:02 +0000

    remove commented out code

--- VaziraniAlgorithm/FINDPATH.m
+++ VaziraniAlgorithm/FINDPATH.m
@@ -15,7 +15,7 @@ if high == low
 end
 
 % unpacking find_path_struct.
-assert(length(fieldnames(find_path_struct))==10);
+assert(length(fieldnames(find_path_struct))==9);
 graph = find_path_struct.graph;
 erased = find_path_struct.erased;
 ownership = find_path_struct.ownership;
@@ -88,113 +88,7 @@ while COA~=low
 end
 
 
-% mark COA visited so we don't have to check it again.
 
-% if COA is in bloom B with wrong parity, back track to parent. (we
-% will have gotten here either via moving to base of bloom or via
-% moving to predecessor with wrong parity).
-
-% if COA is in bloom B with right parity, get possible predecessors. If possible
-% predecessors not empty, move to one of them. If not, back up to parent
-% of COA.
-
-% if COA is in a different bloom from B, move to the base of that
-% bloom if the base is unvisited, and otherwise backtrack.
-
-%     while isempty(preds)
-%         preds = get_possible_predecessors(vx);
-%         vx = parent(vx);
-%     end
-%     for i = 1:length(preds)
-%         u = preds(i);
-%         if ~in_same_bloom(u,B);
-%             [final, base_trail] = trace_bases(u,B);
-%             if final==low || has_same_parity(final,P,ownership)
-%                 parents = insert_base_trail_into_parents(u,base_trail,parents);
-%             end
-%
-%         elseif in_same_bloom(u,B,bloom) && has_same_parity(u,P,ownership);
-%             % we are travelling within B along the correct parity
-%             parent(u) = vx;
-%             vx = u;
-%             break;
-%
-%         end
-%         visited(u) = true;
-%     end
-% end
-
-% while vx ~= low
-%     if vx == 377734
-%         1;
-%         my_tree_plot(parent);
-%     end
-%
-%     history = [history,vx];
-%
-%     % suitable predecessors: we only want to travel along a single
-%     % left/right designation in one call of findpath. in this case, say we
-%     % are only interested in left. if the current vertex has a 'left'
-%     % unvisited predecessor, we just head to that one. if the current
-%     % vertex is in a bloom that is not B, which has a base that is also
-%     % 'left', we move to it. if it is in a bloom that has a base that is
-%     % 'right', we want to mark the base as 'visited', but not go to it. if
-%     % if there are no candidates, we backtrack, else if we are
-%     % in a suitable bloom we go to the base of the bloom, else if we are in
-%     % the specified bloom or no bloom at all, we move to one of the
-%     % unvisited predecessors. in either the base of bloom or the
-%     % predecessor cases, we mark parents. when we arrive at a new vx we
-%     % mark it as visited. we stop once we get to low
-%
-%     if ~isnan(bloom(vx)) && bloom(vx)~=B
-%         % jump to base of bloom if base hasn't been visited and has the
-%         % correct ownership. we are only interested in the ownership of
-%         % v_start because if high is in a bloom, we will emerge at that
-%         % bloom's base regardless of the ownership of high
-%         b = base(bloom(vx));
-%         b_star = base_star(bloom(vx),bloom,base);
-%         if ownership(b_star) == ownership(v_start) && ~visited_vertices(b)
-%             parent(b) = vx;
-%             vx = b;
-%             visited_vertices(b) = true;
-%         else
-%             % the search is depth first, so if we've visited b before and
-%             % not found an augmenting path, we don't need to visit b again.
-%             % and if the base of b belongs to the other 'left/right' we
-%             % want no part.
-%             vx = parent(vx);
-%         end
-%     else
-%         %get the candidates predecessors.
-%         preds = predecessors{vx};
-%         for u = predecessors{vx}
-%             % many cases here. we obviously don't want to look at erased
-%             % vertices or visited vertices (depth first means we've
-%             % exhausted these). We don't want to look at predecessors with
-%             % the wrong left/right as well, unless we are in the bloom B
-%             % and we are jumping to its base, which of course will have a
-%             % different mark. TODO double check that this is always going
-%             % to be B, and clean up if possible. TODO make this only a
-%             % single predecessor. TODO maybe do all of this work at the
-%             % beginning and put into a cell to avoid repeating it.
-%             if erased(u) || visited_vertices(u) || ...
-%                     (ownership(u)~=ownership(v_start) && ...
-%                     (~isnan(bloom(vx)) && u~=base(bloom(vx))));
-%                 preds(preds==u) = [];
-%             end
-%         end
-%         if isempty(preds) % backtrack.
-%             vx = parent(vx);
-%         else % travel down
-%             ux = preds(1);
-%             parent(ux) = vx;
-%             vx = ux;
-%             visited_vertices(vx) = true;
-%         end
-%     end
-%
-% end
-%
 
 % create path from high to low via parent pointers.
 vx = low;
